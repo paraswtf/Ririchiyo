510dc7ca7ae573a6efadce1cb5662569d3b48aab
ParasDeshpande <parasbpndeshpande@gmail.com>
2021-07-27 08:41:45 +0530

    Add pause and resume commands

---
+++ src/commands/music/pause.ts
@@ -0,0 +1,43 @@
+import { BaseCommand } from '../../structures/Commands/BaseCommand';
+import { GuildCTX } from '../../structures/Commands/CTX';
+import { MusicUtil, Error, Success, FLAG } from '../../structures/Utils/MusicUtil';
+import { Util as DCUtil, MessageEmbed } from 'discord.js';
+import { CustomEmojiUtils, EmbedUtils, ThemeUtils } from '../../structures/Utils';
+
+export default class PauseCommand extends BaseCommand {
+    constructor() {
+        super({
+            name: "pause",
+            category: "music",
+            description: "Pause the player",
+            allowSlashCommand: true,
+            allowMessageCommand: true,
+            allowGuildCommand: true,
+            allowDMCommand: false,
+            allowMessageCommponentInteraction: true
+        })
+    }
+
+    async run(ctx: GuildCTX, opts?: Success | Error) {
+        const res = MusicUtil.canPerformAction({
+            guild: ctx.guild,
+            member: ctx.member,
+            ctx,
+            requiredPermissions: ["MANAGE_PLAYER"],
+            memberPermissions: ctx.guildSettings.permissions.members.getFor(ctx.member).calculatePermissions()
+        });
+        if (res.isError) return;
+
+        if (res.dispatcher && res.dispatcher.player.paused) return await ctx.reply({ embeds: [EmbedUtils.embedifyString(ctx.guild, "The player is already paused!", { isError: true })] });
+        if (!res.dispatcher?.playing || !res.dispatcher.queue.current) return await ctx.reply({ embeds: [EmbedUtils.embedifyString(ctx.guild, "There is nothing playing right now!", { isError: true })] });
+
+        res.dispatcher?.player.setPaused(true);
+
+        await res.dispatcher.playingMessages.get(res.dispatcher.queue.current.id)?.setPause(true);
+
+        const options = { embeds: [EmbedUtils.embedifyString(ctx.guild, `${ctx.author} Paused the player!`)] };
+
+        await ctx.reply(options);
+        if (res.dispatcher?.textChannel && ctx.channel.id !== res.dispatcher.textChannel.id) await res.dispatcher.textChannel.send(options);
+    }
+}
---
+++ src/commands/music/resume.ts
@@ -0,0 +1,43 @@
+import { BaseCommand } from '../../structures/Commands/BaseCommand';
+import { GuildCTX } from '../../structures/Commands/CTX';
+import { MusicUtil, Error, Success, FLAG } from '../../structures/Utils/MusicUtil';
+import { Util as DCUtil, MessageEmbed } from 'discord.js';
+import { CustomEmojiUtils, EmbedUtils, ThemeUtils } from '../../structures/Utils';
+
+export default class ResumeCommand extends BaseCommand {
+    constructor() {
+        super({
+            name: "resume",
+            category: "music",
+            description: "Resume the player",
+            allowSlashCommand: true,
+            allowMessageCommand: true,
+            allowGuildCommand: true,
+            allowDMCommand: false,
+            allowMessageCommponentInteraction: true
+        })
+    }
+
+    async run(ctx: GuildCTX, opts?: Success | Error) {
+        const res = MusicUtil.canPerformAction({
+            guild: ctx.guild,
+            member: ctx.member,
+            ctx,
+            requiredPermissions: ["MANAGE_PLAYER"],
+            memberPermissions: ctx.guildSettings.permissions.members.getFor(ctx.member).calculatePermissions()
+        });
+        if (res.isError) return;
+
+        if (res.dispatcher && res.dispatcher.playing) return await ctx.reply({ embeds: [EmbedUtils.embedifyString(ctx.guild, "The player is already playing!", { isError: true })] });
+        if (!res.dispatcher?.player.track || !res.dispatcher.queue.current) return await ctx.reply({ embeds: [EmbedUtils.embedifyString(ctx.guild, "There is nothing playing right now!", { isError: true })] });
+
+        res.dispatcher?.player.setPaused(false);
+
+        await res.dispatcher.playingMessages.get(res.dispatcher.queue.current.id)?.setPause(false);
+
+        const options = { embeds: [EmbedUtils.embedifyString(ctx.guild, `${ctx.author} Resumed the player!`)] };
+
+        await ctx.reply(options);
+        if (res.dispatcher?.textChannel && ctx.channel.id !== res.dispatcher.textChannel.id) await res.dispatcher.textChannel.send(options);
+    }
+}
