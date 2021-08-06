b75bba21ceac9390fe16c0caa1dd62ff56c35672
ParasDeshpande <parasbpndeshpande@gmail.com>
2021-08-06 19:42:46 +0530

    Fix no dispatcher error in disconnect command

--- src/commands/music/disconnect.ts
+++ src/commands/music/disconnect.ts
@@ -3,6 +3,7 @@ import { GuildCTX } from '../../structures/Commands/CTX';
 import { MusicUtil } from '../../structures/Utils/MusicUtil';
 import { EmbedUtils } from '../../structures/Utils';
 import { ApplicationCommandData } from 'discord.js';
+import { inspect } from 'util';
 
 export default class DisconnectCommand extends BaseCommand<true, false> {
     constructor() {
@@ -16,6 +17,7 @@ export default class DisconnectCommand extends BaseCommand<true, false> {
     }
 
     async run(ctx: GuildCTX<false>) {
+        let dispatcher = this.client.dispatchers.get(ctx.guild.id);
         const res = MusicUtil.canPerformAction({
             guild: ctx.guild,
             member: ctx.member,
@@ -24,6 +26,12 @@ export default class DisconnectCommand extends BaseCommand<true, false> {
             memberPermissions: ctx.guildSettings.permissions.members.getFor(ctx.member).calculatePermissions()
         });
         if (res.isError) return;
+
+        if (!dispatcher) dispatcher = res.dispatcher;
+        if (!dispatcher) return await ctx.reply({
+            embeds: [EmbedUtils.embedifyString(ctx.guild, "There is nothing playing right now!", { isError: true })],
+            ephemeral: true
+        });
 
         await this.client.dispatchers.destroy(ctx.guild.id);
 
@@ -34,7 +42,7 @@ export default class DisconnectCommand extends BaseCommand<true, false> {
         };
 
         await ctx.reply(options);
-        if (res.dispatcher!.textChannel && ctx.channel.id !== res.dispatcher!.textChannel.id) await res.dispatcher!.sendMessage(options);
+        if (dispatcher.textChannel && ctx.channel.id !== dispatcher.textChannel.id) await dispatcher.sendMessage(options);
 
     }
 
